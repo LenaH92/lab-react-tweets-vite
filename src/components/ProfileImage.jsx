2d917748000dfd9bfd6af2ec256a64b32be16e0e
Lena Cortes <lena.cortes@usal.es>
2024-12-10 23:54:56 +0100

    iteration3 done

---
+++ src/components/ProfileImage.jsx
@@ -0,0 +1,9 @@
+function profilePicture({profileIMG}){ //passing as an argument what the teewt will put
+    return (<img
+        src={image} 
+        className="profile"
+        alt="profile"
+      />)
+}
+
+export default profilePicture;
